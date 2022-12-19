25d12e5128584d18a3df025bd21f1b79822f8645
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2022-12-19 16:56:52 +0100

    vite somewhat working

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,13 +1,15 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 export default defineConfig({
     main: {
         build: {
             outDir: './build/electron',
+            copyPublicDir: false,
             lib: {
-                entry: 'src/electron',
+                entry: 'src/electron/electron.ts',
                 fileName: 'electron.js'
             }
         },
@@ -16,19 +18,21 @@ export default defineConfig({
     preload: {
         build: {
             outDir: './build/shared',
+            copyPublicDir: false,
             lib: {
-                entry: 'src/shared',
+                entry: 'src/shared/preload.ts',
                 fileName: 'preload.js'
             }
         },
         plugins: [externalizeDepsPlugin()]
     },
     renderer: {
+        root: 'src/ui',
         build: {
-            outDir: './build/ui',
-            lib: {
-                entry: 'src/ui',
-            }
+            outDir: path.join(__dirname, '/build/ui'),
+            rollupOptions: {
+                input: 'src/ui/index.html'
+            },
         },
         resolve: {
             alias: {
