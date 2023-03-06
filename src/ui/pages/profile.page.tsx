2439450260dad0848ce4bf8cd5a725425f3d9690
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2023-03-06 17:35:12 +0100

    testing zod implementation

--- src/ui/pages/profile.page.tsx
+++ src/ui/pages/profile.page.tsx
@@ -1,14 +1,25 @@
 import styled from 'styled-components';
 import ContentBox from '../components/contentBox.component';
 import FormBuilderComponent from '../components/form/formBuilder.component';
+import { useEffect } from 'react';
+import { FormMeta } from 'cmap2-shared';
 
 export default function ProfilePage() {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch('http://localhost:8080/api/form/profile/soawkward', {method: 'GET'});
+            const resData = await res.json() as FormMeta;
+            console.log('Recieved data from server for form: ', resData);
+        };
+        fetchData();
+    }, []);
 
     return (
         <ProfilePageStyled>
             <ContentBox flex={1}><img src={''} alt="Profile picture" /></ContentBox>
             <ContentBox>
-                <FormBuilderComponent formName={'profile'} showLabel={true} />
+                {/* <FormBuilderComponent formName={'profile'} showLabel={true} /> */}
             </ContentBox>
             <ContentBox flexBasis={'100%'}>
                 Donec ac enim porttitor, pretium est vel, mattis eros. Quisque et nibh ac urna hendrerit fringilla in non nulla. Sed facilisis metus
