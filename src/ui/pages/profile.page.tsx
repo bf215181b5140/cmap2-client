6d209434ff8edaaaeab8d78d87dd9514bec161b4
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2023-03-08 19:19:20 +0100

    dropped form builder in WIP on pages

--- src/ui/pages/profile.page.tsx
+++ src/ui/pages/profile.page.tsx
@@ -1,25 +1,29 @@
 import styled from 'styled-components';
 import ContentBox from '../components/contentBox.component';
-import FormBuilderComponent from '../components/form/formBuilder.component';
-import { useEffect } from 'react';
-import { FormMeta } from 'cmap2-shared';
+import { useContext, useEffect, useState } from 'react';
+import { ClientCredentialsContext } from '../App';
 
 export default function ProfilePage() {
 
+    const clientCredentials = useContext(ClientCredentialsContext);
+    const [profileData, setProfileData] = useState<any>();
+
     useEffect(() => {
         const fetchData = async () => {
-            const res = await fetch('http://localhost:8080/api/form/profile/soawkward', {method: 'GET'});
-            const resData = await res.json() as FormMeta;
-            console.log('Recieved data from server for form: ', resData);
+            const res = await fetch(clientCredentials.serverUrl + '/api/profile/' + clientCredentials.username, {method: 'GET'});
+            const resData = await res.json();
+            console.log('Recieved /api/profile/readProfile: ', resData);
+            setProfileData(resData);
         };
         fetchData();
     }, []);
 
     return (
         <ProfilePageStyled>
-            <ContentBox flex={1}><img src={''} alt="Profile picture" /></ContentBox>
+            <ContentBox flex={1}><img src={profileData?.picture} alt="Profile picture" /></ContentBox>
             <ContentBox>
-                {/* <FormBuilderComponent formName={'profile'} showLabel={true} /> */}
+                {/* {profileData?.form && <FormBuilderComponent formMeta={profileData?.form} showLabel={true} */}
+                {/*                        postUrl={clientCredentials.serverUrl + '/api/profile/saveProfile/' + clientCredentials.username} />} */}
             </ContentBox>
             <ContentBox flexBasis={'100%'}>
                 Donec ac enim porttitor, pretium est vel, mattis eros. Quisque et nibh ac urna hendrerit fringilla in non nulla. Sed facilisis metus
