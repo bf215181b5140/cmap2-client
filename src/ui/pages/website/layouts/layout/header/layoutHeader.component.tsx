c20cbcad450a16e3e70d719b28c34fc4e14c2b62
bf215181b5140 <108577982+bf215181b5140@users.noreply.github.com>
2025-03-13 16:56:23 +0100

    layout header

--- src/ui/pages/website/layouts/layout/header/layoutHeader.component.tsx
+++ src/ui/pages/website/layouts/layout/header/layoutHeader.component.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
-import { LayoutDTO } from 'cmap2-shared';
+import { LayoutDTO, ParameterBadgeTypeSchema } from 'cmap2-shared';
 import { useNavigate, useParams } from 'react-router-dom';
 import AddCounter from '../../../../../components/addCounter/addCounter.component';
 import AddNewButton from '../../../../../components/addNewButton/addNewButton.component';
 import { SelectInputStyled } from '../../../../../components/input/input.style';
 import { useContext } from 'react';
 import { LayoutsPageContext } from '../../layouts.context';
+import { HealthAndExp, ParameterBadge } from 'cmap2-shared/react';
 
 interface LayoutProps {
   layout?: LayoutDTO;
@@ -19,6 +20,41 @@ export default function LayoutHeader({ layout }: LayoutProps) {
 
   const canAddLayout = layouts.length <= tier.layouts;
 
+  const health = Math.floor(Math.random() * 20);
+  const exp = Math.floor(Math.random() * 100);
+
+  function parameterBadgeMap() {
+    const map = new Map<string, string | number | boolean>();
+    layout?.parameterBadges?.forEach(badge => {
+      switch (badge.type) {
+        case ParameterBadgeTypeSchema.Values.Afk:
+          map.set(badge.path, true);
+          break;
+        case ParameterBadgeTypeSchema.Values.VrMode:
+          map.set(badge.path, 1);
+          break;
+        case ParameterBadgeTypeSchema.Values.Mute:
+          map.set(badge.path, true);
+          break;
+        case ParameterBadgeTypeSchema.Values.TrackingType:
+          map.set(badge.path, 6);
+          break;
+        case ParameterBadgeTypeSchema.Values.Custom:
+          if (badge.label.includes('{v}')) {
+            map.set(badge.path, Math.floor(Math.random() * 20));
+            break;
+          }
+          if (badge.value !== null) {
+            map.set(badge.path, badge.value);
+            break;
+          }
+          break;
+      }
+
+    })
+    return map;
+  }
+
   return (<LayoutHeaderStyled>
     <SelectedLayoutStyled onClick={() => navigate(`/website/layouts/edit/layout/${layout?.id}`)} aria-disabled={!layout?.id}>
 
@@ -26,14 +62,8 @@ export default function LayoutHeader({ layout }: LayoutProps) {
         {layouts.map(layout => (<option key={layout.id} value={layout.id}>{layout.label}</option>))}
       </SelectInputStyled>
 
-      <div>
-        {layout?.parameterBadges?.map(parameterBadge => (<span key={parameterBadge.id} style={{ marginRight: '10px' }}>{parameterBadge.type}</span>))}
-      </div>
-
-      <div>
-        {layout?.useCostEnabled && <div>Use cost</div>}
-        {layout?.healthEnabled && <div>Health</div>}
-      </div>
+      {(layout?.parameterBadges?.length || 0) > 0 && <div>{layout?.parameterBadges?.map(badge => <ParameterBadge key={badge.id} badge={badge} parameters={parameterBadgeMap()} />)}</div>}
+      {layout && <HealthAndExp layout={layout} health={health} exp={exp} />}
 
     </SelectedLayoutStyled>
 
@@ -61,7 +91,7 @@ const SelectedLayoutStyled = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 12px;
   cursor: pointer;
   transition: 0.1s linear;
 
